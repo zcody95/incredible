9dcdb2d120c5642deb96f0724dee0efd755f9c6e
rspatric <roslyn.sierra@gmail.com>
2017-02-22 13:34:14 -0800

    Cleaned up APIs and set up final APIs for articles.

--- src/com/calpoly/incredible/Incredible.java
+++ src/com/calpoly/incredible/Incredible.java
@@ -4,113 +4,166 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-
+import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Scanner;
 
+/**
+ * This class runs all of our APIs on the article.
+ * @author roslynsierra
+ */
 public class Incredible {
-    private static String connectionUrl;
-    private static Connection con;
+   private static String connectionUrl;     
+   private static Connection con;
 
-    public static void main(String[] args) throws UnirestException {
+   /*
+    * This is the main method that runs all the APIs
+    * @param args the commanline arguments. args[0] must be a url passed
+    * in by the user.
+    **/
+   public static void main(String[] args) throws UnirestException {
+      // Connects to the Azure Backend using JDBC
+      try {
+         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+         connectionUrl = "jdbc:sqlserver://incredibleserver17.database.wi    ndows.net:1433;database=IncredibleStorage;" +
+            "user=aibackend@outlook.com@incredibleserver17;" +
+            "password={Backend123!};encrypt=true;" +
+            "trustServerCertificate=false;" +
+            "hostNameInCertificate=*.database.windows.net;loginTimeo    ut=30;";
+         con = DriverManager.getConnection(connectionUrl);
+         System.out.println("Successfully connected to SQL database");
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
 
-        // Connects to the Azure Backend using JDBC
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connectionUrl = "jdbc:sqlserver://incredibleserver17.database.windows.net:1433;database=IncredibleStorage;" +
-                    "user=aibackend@outlook.com@incredibleserver17;" +
-                    "password={Backend123!};encrypt=true;" +
-                    "trustServerCertificate=false;" +
-                    "hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-            con = DriverManager.getConnection(connectionUrl);
-            System.out.println("Successfully connected to SQL database");
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
+      //check that the user passed in a url for the APIs 
+      if (!(args.length >= 1)) {
+         System.out.println("ERROR: Must provide a url.");
+         return;
+      }
+      String url = args[0];
+      //Textuality API
+      //Textuality pulls out important information from the link provided.
+      //It will provide the title, content, links on the page, and images.
 
+      // These code snippets use an open-source library. http://unirest.io/java
+      HttpResponse<JsonNode> response = Unirest.post("https://extracttext.p.mashape.com/api/content_extract/")
+         .header("X-Mashape-Key", "6mHWrpJfngmshbsZcedi1XmR2Urbp1kHUCgjsnctb0yJ4Ezskf")
+         .header("Content-Type", "application/x-www-form-urlencoded")
+         .header("Accept", "application/json")
+         .field("dataurl", url)
+         .asJson();
 
+      //get title of article
+      String title = response.getBody().getObject().get("title").toString();
+      System.out.println("Title: " + title);
+      System.out.println("");
+      //get body of article
+      String body = response.getBody().getObject().get("content").toString();
 
-        //HttpResponse<String> request = Unirest.get("https://yoda.p.mashape.com/yoda?sentence=You%20will%20learn%20how%20to%20speak%20like%20me%20someday.%20%20Oh%20wait.")
-        //.header("X-Mashape-Authorization", "<Insert your Mashape key here>").asString();
+      //Skittle 2.0 API
+      //This pulls out the most opinionated sentences and determines if they have
+      //positive or negative connotation. It also pulls out the most common terms.
 
-        //System.out.println(request.getBody());
+      // These code snippets use an open-source library.
+      response = Unirest.post("https://sentinelprojects-skyttle20.p.mashape.com/")
+         .header("X-Mashape-Key", "6mHWrpJfngmshbsZcedi1XmR2Urbp1kHUCgjsnctb0yJ4Ezskf")
+         .header("Content-Type", "application/x-www-form-urlencoded")
+         .header("Accept", "application/json")
+         .field("annotate", 1)
+         .field("keywords", 1)
+         .field("lang", "en")
+         .field("sentiment", 1)
+         .field("text", body)
+         .asJson();
 
-        if (!(args.length >= 1)) {
-            System.out.println("ERROR: Must provide a url.");
-            return;
-        }
+      //find the top three most common terms in article
+      String term1 = "", term2 = "", term3 = "";
+      int count1 = 0, count2 = 0, count3 = 0;
 
-        String url = args[0];
+      JSONArray docs = response.getBody().getObject().getJSONArray("docs");
+      JSONObject json = (JSONObject) docs.getJSONObject(0);
+      JSONArray terms = json.getJSONArray("terms");
 
-        System.out.println("POST Textuality");
-        //Textuality pulls out important information from the link provided.
-        //It will provide the title, content, links on the page, and images.
+      for (int i = 0; i < terms.length(); i++) {
+         JSONObject term = (JSONObject) terms.get(i);
+         int current = (Integer) term.get("count");
+         if (current > count1 && current > count2 && current > count3) {
+            count3 = count2;
+            term3 = term2;
+            count2 = count1;
+            term2 = term1;
+            count1 = current;
+            term1 = (String) term.get("term");
+         }
+         else if (current > count2 && current > count3) {
+            count3 = count2;
+            term3 = term2;
+            count2 = current;
+            term2 = (String) term.getString("term");
+         }
+         else if (current > count3) {
+            count3 = current;
+            term3 = (String) term.getString("term");
+         }
 
-        // These code snippets use an open-source library. http://unirest.io/java
-        HttpResponse<JsonNode> response = Unirest.post("https://extracttext.p.mashape.com/api/content_extract/")
-                .header("X-Mashape-Key", "6mHWrpJfngmshbsZcedi1XmR2Urbp1kHUCgjsnctb0yJ4Ezskf")
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .header("Accept", "application/json")
-                .field("dataurl", url)
-                .asJson();
+      }
+      System.out.println("Most common term is " + term1);
+      System.out.println("Second most common term is " + term2);
+      System.out.println("Third most common term is " + term3);
+      System.out.println("");
 
-        System.out.println("AFTER POST");
-        System.out.println(response.getBody().toString());
-        String text = response.getBody().toString();
-        Scanner s = new Scanner(text).useDelimiter(",");
+      //Semantic Relatedness API
+      //Semantic Relatedness compares how related the two bodies of text are.
+      //This could be used to compare titles and find similar articles
 
-        while (s.hasNext()) {
-            System.out.println(s.next());
-        }
+      // These code snippets use an open-source library. http://unirest.io/java
+      response = Unirest.post("https://amtera.p.mashape.com/relatedness/en")
+         .header("X-Mashape-Key", "6mHWrpJfngmshbsZcedi1XmR2Urbp1kHUCgjsnctb0yJ4Ezskf")
+         .header("Content-Type", "application/json")
+         .header("Accept", "application/json")
+         .body("{\"t1\":\""+title+"\",\"t2\":\"Put other title here\"}")
+         .asJson();
 
-        System.out.println("");
-        //get title of article
-        String title = response.getBody().getObject().get("title").toString();
-        System.out.println("Title: "+title);
+      System.out.println("Semantic Relatedness Score: " + response.getBody().getObject().get("v"));
 
-        System.out.println("");
-        System.out.println("GET Alchemy");
-        //Alchemy pulls out the title. Not sure if we want to use this API. It
-        //doesn't seem to be quite as useful or easy to use as Textuality
+      //Grammar and Spell Checking
+      //Most credible articles will have very good grammar and spelling.
+      //Those that don't are likely not as credible of sources, because the 
+      //author or editor was not knowledgable enough to write correctly.
 
-        // These code snippets use an open-source library. http://unirest.io/java
-        response = Unirest.get("https://alchemy.p.mashape.com/url/URLGetFeedLinks?outputMode=json&url="+url)
-                .header("X-Mashape-Key", "6mHWrpJfngmshbsZcedi1XmR2Urbp1kHUCgjsnctb0yJ4Ezskf")
-                .header("Accept", "application/json")
-                .asJson();
+      System.out.println("");
+      //Language Tool API
+      // These code snippets use an open-source library. http://unirest.io/java
+      HttpResponse<String> spelling = Unirest.post("https://dnaber-languagetool.p.mashape.com/v2/check")
+         .header("X-Mashape-Key", "6mHWrpJfngmshbsZcedi1XmR2Urbp1kHUCgjsnctb0yJ4Ezskf")
+         .header("Content-Type", "application/x-www-form-urlencoded")
+         .header("Accept", "text/plain")
+         .field("language", "en-US")
+         .field("text", body)
+         .asString();
 
-        System.out.println("AFTER GET");
-        System.out.println(response.getBody().toString());
-        text = response.getBody().toString();
-        s = new Scanner(text).useDelimiter(",");
+      //count number of mistakes
+      String str = spelling.getBody();
+      String findStr = "\"message\":";
+      int lastIndex = 0;
+      int count = 0;
+      while(lastIndex != -1) {
+         lastIndex = str.indexOf(findStr,lastIndex);
+         if(lastIndex != -1) {
+            count ++;
+            lastIndex += findStr.length();
+         }
+      }
+      System.out.println("There were " + count + " errors");
 
-        while (s.hasNext()) {
-            System.out.println(s.next());
-        }
-
-        System.out.println("");
-        System.out.println("POST Semantic Relatedness");
-        //Semantic Relatedness compares how related the two bodies of text are.
-        //I'm not yet sure how this will work will long bodies of text, but it
-        //could be used to compare titles of articles and then find credible
-        //articles about the same subject for fact checking purposes.
-
-        // These code snippets use an open-source library. http://unirest.io/java
-        response = Unirest.post("https://amtera.p.mashape.com/relatedness/en")
-                .header("X-Mashape-Key", "6mHWrpJfngmshbsZcedi1XmR2Urbp1kHUCgjsnctb0yJ4Ezskf")
-                .header("Content-Type", "application/json")
-                .header("Accept", "application/json")
-                .body("{\"t1\":\""+title+"\",\"t2\":\"Put other title here\"}")
-                .asJson();
-
-        System.out.println("POST POST");
-        System.out.println(response.getBody().toString());
-        text = response.getBody().toString();
-        s = new Scanner(text).useDelimiter(",");
-
-        while (s.hasNext()) {
-            System.out.println(s.next());
-        }
-    }
+      //get number of words
+      String wordcount = spelling.getHeaders().get("Content-Length").toString();
+      System.out.println("Number of words is " + wordcount);
+      wordcount = wordcount.substring(1, wordcount.length() - 1);
+      int words = Integer.parseInt(wordcount);
+      double errorPercentage = (double) count / (double) words;
+      System.out.println("Percentage of error is " + errorPercentage);
+   }
 }
